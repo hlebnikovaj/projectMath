7a453cceae076c4b00e738b214c50aaea05a1efa
hlebnikovaj <matcveiko@yandex.ru>
2023-05-14 12:06:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectmath/Example.java
@@ -0,0 +1,14 @@
+package com.example.projectmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Example extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
---
+++ app/src/main/java/com/example/projectmath/Ustnij_schet.java
@@ -0,0 +1,14 @@
+package com.example.projectmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ustnij_schet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ustnij_schet);
+    }
+}
