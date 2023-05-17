4cf0e8cdacbceb548e191c129923fbcbaa374eb9
hlebnikovaj <matcveiko@yandex.ru>
2023-05-18 00:11:44 +0700

    18 may

--- app/src/main/java/com/example/projectmath/oralAccauntFirst.java
+++ app/src/main/java/com/example/projectmath/oralAccauntFirst.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Ustnij_schet extends AppCompatActivity {
+public class oralAccauntFirst extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ustnij_schet);
+        setContentView(R.layout.activity_oral_accaunt_first);
     }
 }
